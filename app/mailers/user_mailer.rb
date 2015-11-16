791ae45ed10db1fc35bc2120aa248af8dfa9db43
fifart <fifart@gmail.com>
2015-11-16 14:14:29 +0000

    Mailer Working

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,11 @@
+class UserMailer < ApplicationMailer
+    
+    def contact_email(contact)
+    
+    @contact = contact 
+    
+    mail(to: Rails.application.secrets.owner_email, from: @contact.email, :subject => "Website Contact")
+    
+    end
+    
+end
